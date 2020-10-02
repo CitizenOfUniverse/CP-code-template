172a8aaf4a4f0f5be0b04aa1c2673f18e7622c85
CitizenOfUniverse <isfilimonov.tgc@gmail.com>
2020-10-02 09:10:06 +0300

    Initial - Basic C++ code sample

---
+++ Source_template.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void problem();
+
+int main() {
+	//Comment these 2 lines if i/o is console (standart i/o)
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	//Making input/output faster for problems
+	//with big constraints
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//Function with solution
+	problem();
+	return 0;
+}
+
+typedef long long ll;
+
+void problem() {
+	//Put your code here
+	
+}
